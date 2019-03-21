0f452c57fa571b38e8ae320f6de0c56c92f7ad94
citizen55 <citizen_55@mail.ru>
2019-03-21 17:24:40 +0700

    Add files via upload

---
+++ res/kinetic3_debug/js/kinetic3_debug.js
@@ -0,0 +1,257 @@
+
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready(){
+
+    var MainScene = new Phaser.Class({
+
+        Extends: Phaser.Scene,
+    
+        initialize:
+    
+        function MainScene (config)
+        {
+            Phaser.Scene.call(this, config)
+    
+            this.cam;
+            this.kinetic;
+
+            this.height = 400;
+        },
+    
+        preload: function ()
+        {
+            this.load.image('grid', '../img/grid_4096.png');
+            this.load.image('crate', '../img/crate.png');
+            this.load.image('sky', '../img/sky4.png');
+
+            this.load.scenePlugin({
+                key: 'KineticScrolling',
+                url: 'js/main.js',
+                sceneKey: 'kinetic'
+            });
+        },
+    
+        create: function ()
+        {
+            this.grid = this.add.image(0, 0, 'sky').setOrigin(0);
+            this.grid.displayHeight = 400;
+            this.cam = this.cameras.main; 
+            this.cameras.main.setViewport(0, 0, 800, 600);
+
+
+            this.scene.add('Bottom', Bottom, true, { x: 0, y: 0, width: 800, height: 200 });
+
+            this.scene.moveDown();
+
+            this.game.events.once('kineticdrag', this.createBox, this);
+        },
+
+        createBox(pointer){
+            this.createImg(pointer);
+            setTimeout(function(){
+                this.game.events.once('kineticdrag', this.createBox, this);
+            }.bind(this), 500);
+        },
+
+        createImg(pointer){
+            //debugger;
+            this.scene.moveUp();
+            var vec2 = pointer.positionToCamera(this.cameras.main);
+            var box = this.add.image(vec2.x, vec2.y, 'crate');
+            // var box = this.add.image(100, 100, 'crate');
+            box.setInteractive();
+            // this.input.setDraggable(box, true);
+
+            box.on('pointermove', function (pointer) {
+                 //debugger;   
+                this.x = pointer.x;
+                this.y = pointer.y;
+        
+            }, box);
+
+            box.on('pointerup', function() {
+                this.off('pointermove');
+            }, box);
+        }
+    });
+
+    var Bottom = new Phaser.Class({
+
+        Extends: Phaser.Scene,
+    
+        initialize:
+    
+        function Bottom ()
+        {
+            Phaser.Scene.call(this, { key: 'Bottom'});
+
+               
+            this.cam;
+            this.kinetic;
+        },
+    
+        preload: function ()
+        {
+            this.load.image('crate', '../img/crate.png');
+            this.load.image('grid', '../img/grid_4096.png');
+        },
+    
+        create: function ()
+        {
+            _this = this;
+            this.beginTime = Date.now();
+            this.txt = '';
+
+
+            
+
+            var sky = this.add.image(2048, 500, 'grid');
+            sky.displayWidth = 4096;
+            this.cam = this.cameras.main;
+            this.cam.setViewport(0, 400, 800, 200);
+            this.cam.setBounds(0, 400, 4096, 600).setZoom(1);
+
+            // this.debugTxt = this.add.text(600, 420, '', {
+            //     fontSize: '22px',
+            //     fill: '#ffffff'
+            // });
+            //this.debugTxt.setScrollFactor(0);
+
+          // this.updateDebugTxt('begin ' + this.beginTime);
+
+            let config = {
+                kineticMovement: true,
+                timeConstantScroll: 325, //really mimic iOS
+                hScroll: true,          // horizontal with pointer
+                vScroll: false,          // vertical
+                hWheel: true,          //horizontal scroll mouse wheel
+                vWheel: false,           //vertiacal 
+                deltaWheel: 20,
+                onUpdate: 0 //(x, y) => {console.log('x=' + x + ', y='+ y)}
+            };
+
+            this.kinetic.start(this.cam, config);
+
+            for(let i = 0; i < 40; i++){
+                let x = i * 100 + 50;
+
+                var box = this.add.image(x, 500, 'crate');
+                box.name = x;
+                box.setInteractive();
+
+                box.bottom = this;
+
+
+                box.on('click', function(box) {
+                    console.log('box: ', box.name);
+                    this.debugText('click on box ' + box.name);
+                   // _this.updateDebugTxt('click on box: ' + box.name);
+                }, this);
+
+                box.onKineticDown = function(pointer){
+                    console.log('down', this.name);
+                    _this.debugText('KineticDown on box '+ this.name);
+                    this.scene.tweens.add({
+                        targets: this,
+                        rotation: 0.5,
+                        duration: 200,
+                        ease: 'Linear'
+                    });
+                }
+
+                box.onKineticUp = function(){
+                    console.log('KineticUp ' + this.name);
+                    _this.debugText('box: KineticUp ' + this.name);
+                    this.scene.tweens.add({
+                        targets: this,
+                        rotation: 0,
+                        duration: 200,
+                        ease: 'Linear'
+                    });
+                }
+
+                box.onKineticClick = function(){
+                    console.log('click ' + this.name);
+                    _this.debugText('box: kineticClick name: ' + this.name);
+                }
+
+                this.kinetic.addInteractive(box);
+                
+                this.input.setDraggable(box);
+
+                this.input.once('dragstart', this.drag, this);
+
+            }
+
+            this.input.dragTimeThreshold = 1500;
+            this.input.dragDistanceThreshold = 24;
+
+            this.input.on('pointerdown', function(pointer){
+                _this.debugText('In scene ' + _this.scene.key + ' ptr.x: ' + pointer.x + ' ptr.y: ' + pointer.y);
+               // _this.updateDebugTxt('pointerdown');
+            })
+        },
+
+        drag(pointer, box){
+            console.log('dragstart', box.name);
+            this.sys.game.events.emit('kineticdrag', pointer);
+            //this.scene.input.emit('kineticdrag', pointer);
+            //this.scene.game.events.emit('kineticdrag', pointer);
+            this.debugText(
+                'box.name: ' + box.name + ' ' + _this.scene.key + ' dragstart ptr.x: ' 
+                + pointer.x + ' ptr.y: ' + pointer.y
+            );
+
+            // setTimeout(function(){
+            //     this.input.once('dragstart', this.drag, this);
+            // }.bind(this), 1000);
+        },
+    
+        update: function (time, delta){
+        },
+
+        debugText(str){
+            var ms = Date.now() - this.beginTime;
+            this.txt += str + ' time: ' + ms + '\n';
+            this.downloadLogFile();
+        },
+
+        downloadLogFile() {
+            var log = 'data:text/plian;charset=utf-8,' + encodeURIComponent(this.txt),
+                elem = document.getElementById('download-btn');
+    
+            if(!elem) {
+                wrapper = document.getElementById('phaser-example');
+                wrapper.setAttribute('style', 'font-size: 30px; position: absolute;');
+                elem = document.createElement("a");
+                elem.id = 'download-btn';
+                elem.style.color = 'white';
+                elem.innerHTML = 'Скачать log';
+                wrapper.appendChild(elem);
+              
+                // document.body.appendChild(wrapper);
+            }
+    
+            elem.setAttribute("href", log);
+            elem.setAttribute("download", 'log.txt');
+        },
+
+        updateDebugTxt (str){
+            this.debugTxt.setText(str);
+        }
+
+    
+    });
+      
+    const config = {
+        title: "Kinetic",
+        version: "1.0",
+        width: 800,
+        height: 600,
+        type: Phaser.AUTO,
+        scene: [MainScene],
+        backgroundColor: "#ffffff"
+    };
+    var game = new Phaser.Game(config);
+}
---
+++ res/kinetic3_debug/js/main.js
@@ -0,0 +1,483 @@
+(function(){
+    var root = this;
+
+class KineticScrolling extends Phaser.Plugins.ScenePlugin{
+
+    constructor(scene, plugimManager){
+
+        super(scene, plugimManager);
+
+        this.pointerId = null;
+        this.dragging = false;
+        this.pressedDown = false;
+        this.timestamp = 0;
+        //this.callbackID = 0;
+
+        this.targetX = 0;
+        this.targetY = 0;
+
+        this.autoScrollX = false;
+        this.autoScrollY = false;
+
+        this.startX = 0;
+        this.startY = 0;
+
+        this.velocityX = 0;
+        this.velocityY = 0;
+
+        this.amplitudeX = 0;
+        this.amplitudeY = 0;
+
+        this.directionWheel = 0;
+
+        this.velocityWheelX = 0;
+        this.velocityWheelY = 0;
+
+        // if less than the two values is a Tap
+        this.thresholdOfTapTime = 150;
+        this.thresholdOfTapDistance = 40; // 10
+
+        // for a smoother scrolling start
+        this.thresholdReached = false;
+
+        this.clickHelperActiveObjects = [];
+
+        this.settings = {
+            kineticMovement: true,
+            timeConstantScroll: 325, //really mimic iOS
+            hScroll: true,          // horizontal with pointer
+            vScroll: true,          // vertical
+            hWheel: false,          //horizontal scroll mouse wheel
+            vWheel: true,           //vertiacal 
+            deltaWheel: 30,
+            onUpdate: 0 //(x, y) => {console.log('x=' + x + ', y='+ y)}
+        };
+
+        this.camera;
+        this.clearMovementTimer = 0; 
+        this.kineticDown = false;
+    }
+
+    boot(){
+        console.log('boot');
+        var eventEmitter = this.systems.events;
+        eventEmitter.on('update', this.update, this);
+    }
+
+    /**
+     * 
+     * @param {Phaser.Camers} camera 
+     * @param {any} config 
+     */
+    start(camera, config){
+
+        if (config) this.configure(config);
+
+        this.eventEmitter = this.scene.events;
+
+        if(camera == undefined){
+            this.camera = scene.cameras.main;
+        }else{
+            this.camera = camera;
+        }
+
+        let c = this.cam;
+
+        //this.zone =  this.scene.add.zone(c.x, c.y, c.width, c.height).setOrigin(0).setInteractive();
+
+        this.eventEmitter = this.game.events;
+        this.scene.input.on('pointerdown', this.beginMove, this);
+        this.scene.input.on('pointermove', this.moveCamera, this);
+        this.scene.input.on('pointerup', this.endMove, this);
+        this.game.events.on('mouseout', this.endMove, this);
+        if (window.addEventListener)
+        {
+            window.addEventListener('DOMMouseScroll', this.mouseWheel.bind(this), false);
+            window.onmousewheel = this.mouseWheel.bind(this);
+        }
+        this.scene.input.on('kineticdrag', () => {
+            this.endMove();
+        })
+    }
+
+    beginMove(pointer) {
+        this.scene.debugText('kinetic beginMove()  pointer.id: ' + pointer.id);
+
+        if(!pointer.camera || pointer.camera.id != this.camera.id){
+            return;
+        }
+        //this.scene.input.on('pointermove', this.moveCamera, this);
+
+        this.pointerId = pointer.id;
+        this.startX = this.game.input.x;
+        this.startY = this.game.input.y;
+        this.screenX = pointer.screenX;
+        this.screenY = pointer.screenY;
+        this.pressedDown = true;
+        this.thresholdReached = false;
+        this.timestamp = Date.now();
+        this.beginTime = this.timestamp;
+        this.velocityY = this.amplitudeY = this.velocityX = this.amplitudeX = 0;
+
+        this.timerDown = setTimeout(() => {
+            if(!this.thresholdReached){
+                this.scene.input.emit('kineticDown', pointer);
+                this.kineticDown = true;
+            }
+        }, this.thresholdOfTapTime + 10);
+    }
+
+    moveCamera(pointer) {
+
+        clearTimeout(this.clearMovementTimer);
+        this.scene.debugText('kinetic move()  pointer.id: ' + pointer.id);
+
+        if(!pointer.camera || pointer.camera.id != this.camera.id){
+            this.endMove(pointer);
+        }
+        if (!this.pressedDown) {
+            return;
+        }
+        // If it is not the current pointer
+        if (this.pointerId !== pointer.id) {
+            return;
+        }
+
+        let x = pointer.x;
+        let y = pointer.y;
+
+        this.now = Date.now();
+        var elapsed = this.now - this.timestamp;
+        this.timestamp = this.now;
+
+        var deltaX = 0;
+        var deltaY = 0;
+
+        // It`s a fast tap not move
+        if ( this.isTap()
+            && Math.abs(pointer.screenY - this.screenY) < this.thresholdOfTapDistance
+            && Math.abs(pointer.screenX - this.screenX) < this.thresholdOfTapDistance
+        ) {
+            return;
+        }
+
+        if (!this.thresholdReached) {
+            this.thresholdReached = true;
+            this.startX = x;
+            this.startY = y;
+            if(this.kineticDown){
+                this.scene.input.emit('kineticUp');
+                this.scene.debugText('kinetic: kineticUp !this.thresholdReached');
+            }else{
+                clearTimeout(this.timerDown);
+                this.kineticDown = false;
+            }
+            this.scene.debugText('kinetic: kineticOff !this.thresholdReached');
+            this.scene.input.emit('kineticOff');
+            
+            return;
+        }
+
+        if (this.settings.hScroll) {
+            deltaX = x - this.startX;
+            if (deltaX !== 0) {
+                this.dragging = true;
+            }
+            this.startX = x;
+            this.velocityX = 0.8 * (1000 * deltaX / (1 + elapsed)) + 0.2 * this.velocityX;
+            this.camera.scrollX -= deltaX;
+        }
+
+        if (this.settings.vScroll) {
+            deltaY = y - this.startY;
+            if (deltaY !== 0) {
+                this.dragging = true;
+            }
+            this.startY = y;
+            this.velocityY = 0.8 * (1000 * deltaY / (1 + elapsed)) + 0.2 * this.velocityY;
+            this.camera.scrollY -= deltaY;
+        }
+
+        if (typeof this.settings.onUpdate === 'function') {
+            var updateX = 0;
+            if (this.canCameraMoveX(this.camera, deltaX)) {
+                updateX = deltaX;
+            }
+
+            var updateY = 0;
+            if (this.canCameraMoveY(this.camera, deltaY)) {
+                updateY = deltaY;
+            }
+
+            this.settings.onUpdate(updateX, updateY);
+        }
+
+        this.clearMovementTimer = setTimeout(function () {
+            this.velocityX = 0;
+            this.velocityY = 0;
+        }.bind(this), 20);
+    }
+
+    /**
+    * Validate if the gesture is a tap
+    * @return {boolean}
+    */
+    isTap() {
+        return (this.now - this.beginTime) < this.thresholdOfTapTime;
+    };
+
+    endMove(pointer) {
+        
+        this.scene.debugText('kinetic endMove()  pointer.id: ' + pointer);
+        
+        if(pointer != undefined && !this.pressedDown){
+            return;
+        }
+
+        clearTimeout(this.timerDown);
+        this.kineticDown = false;
+        this.scene.debugText('kinetic: kineticUp endMove');
+        this.scene.input.emit('kineticUp');
+        this.scene.debugText('kinetic: kineticClick endMove');
+        this.scene.input.emit('kineticClick');
+        this.scene.debugText('kinetic: kineticOff endMove');
+        this.scene.input.emit('kineticOff');
+        
+
+        clearTimeout(this.clearMovementTimer);
+        this.pointerId = null;
+        this.pressedDown = false;
+        this.autoScrollX = false;
+        this.autoScrollY = false;
+
+        if (!this.settings.kineticMovement) return;
+
+        this.now = Date.now();
+
+        let over = pointer && pointer.camera && pointer.camera.id == this.camera.id;
+
+        if (over) {     //this.game.isOver
+            if (this.velocityX > 10 || this.velocityX < -10) {
+                this.amplitudeX = 0.8 * this.velocityX;
+                this.targetX = Math.round(this.camera.scrollX - this.amplitudeX);
+                this.autoScrollX = true;
+            }
+
+            if (this.velocityY > 10 || this.velocityY < -10) {
+                this.amplitudeY = 0.8 * this.velocityY;
+                this.targetY = Math.round(this.camera.scrollY - this.amplitudeY);
+                this.autoScrollY = true;
+            }
+        }
+        if (!over) {
+            this.velocityWheelXAbs = Math.abs(this.velocityWheelX);
+            this.velocityWheelYAbs = Math.abs(this.velocityWheelY);
+            if ( this.settings.hScroll) {
+                this.autoScrollX = true;
+            }
+            if (this.settings.vScroll) {
+                this.autoScrollY = true;
+            }
+        }
+    }
+
+    update(time, elapsed) {
+
+        this.elapsed = Date.now() - this.timestamp;
+        
+        this.velocityWheelXAbs = Math.abs(this.velocityWheelX);
+        this.velocityWheelYAbs = Math.abs(this.velocityWheelY);
+
+        var delta = 0;
+        if (this.autoScrollX && this.amplitudeX != 0) {
+
+            delta = -this.amplitudeX * Math.exp(-this.elapsed / this.settings.timeConstantScroll);
+            if (this.canCameraMoveX(this.camera, delta) && (delta > 0.5 || delta < -0.5)) {
+                this.camera.scrollX = this.targetX - delta;
+            }
+            else {
+                this.autoScrollX = false;
+                this.camera.scrollX = this.targetX;
+            }
+        }
+
+        if (this.autoScrollY && this.amplitudeY != 0) {
+
+            delta = -this.amplitudeY * Math.exp(-this.elapsed / this.settings.timeConstantScroll);
+            if (this.canCameraMoveY(this.camera, delta) && (delta > 0.5 || delta < -0.5)) {
+                this.camera.scrollY = this.targetY - delta;
+            }
+            else {
+                this.autoScrollY = false;
+                this.camera.scrollY = this.targetY;
+            }
+        }
+
+        if (!this.autoScrollX && !this.autoScrollY) {
+            this.dragging = false;
+        }
+
+        if (this.settings.hWheel && this.velocityWheelXAbs > 0.1) {
+            this.dragging = true;
+            this.amplitudeX = 0;
+            this.autoScrollX = false;
+            this.camera.scrollX -= this.velocityWheelX;
+            this.velocityWheelX *= 0.95;
+        }
+
+        if (this.settings.vWheel && this.velocityWheelYAbs > 0.1) {
+            this.dragging = true;
+            this.autoScrollY = false;
+            this.camera.scrollY -= this.velocityWheelY;
+            this.velocityWheelY *= 0.95;
+        }
+    };
+
+    /**
+    * Indicates when camera can move in the x axis
+    * @return {boolean}
+    */
+    canCameraMoveX(cam, delta) {
+        let bounds = cam.getScroll(cam.scrollX + delta, 0);
+        if(cam.scrollX != bounds.x){
+            return true;
+        }
+        return false;
+    };
+
+          /**
+    * Indicates when camera can move in the y axis
+    * @return {boolean}
+    */
+    canCameraMoveY(cam, delta) {
+        let bounds = cam.getScroll(0, cam.scrollY + delta);
+        if(cam.scrollY != bounds.x){
+            return true;
+        }
+        return false;
+    };
+
+    /**
+    * Event called when the mousewheel is used, affect the direction of scrolling.
+    */
+    mouseWheel(event) {
+
+        if (!this.settings.hWheel && !this.settings.vWheel) return;
+
+        event.preventDefault();
+        let delta = 0;
+        let wheelDelta = 0;
+
+        wheelDelta = event.wheelDelta || -event.detail;
+        wheelDelta = Math.max(-1, Math.min(1, wheelDelta));
+        delta = wheelDelta * 120 / this.settings.deltaWheel;
+
+        if (this.directionWheel != wheelDelta) {
+            this.velocityWheelX = 0;
+            this.velocityWheelY = 0;
+            this.directionWheel = wheelDelta;
+        }
+
+        let cam = this.camera;
+
+        if (this.settings.hWheel) {
+            this.autoScrollX = false;
+
+            this.velocityWheelX += delta;
+
+            if (typeof this.settings.onUpdate === 'function') {
+                var deltaX = 0;
+                if(this.canCameraMoveX(cam, delta)){
+                    deltaX = delta;
+                }
+
+                this.settings.onUpdate(deltaX, 0);
+            }
+        }
+        
+        if (this.settings.vWheel) {
+            this.autoScrollY = false;
+
+            this.velocityWheelY += delta;
+
+            if (typeof this.settings.onUpdate === 'function') {
+                var deltaY = 0;
+                if(this.canCameraMoveY(cam, delta)){
+                    deltaY = delta;
+                }
+                this.settings.onUpdate(0, deltaY);
+            }
+        }
+    };
+
+    addInteractive(gameobject){
+        gameobject.setInteractive();
+        gameobject.on('pointerdown', function(pointer){
+            this.bottom.debugText('\npointerdown  subscribe box on kinetic events  ptr.x: ' + pointer.x + ' ptr.y: ' + pointer.y);
+            this.bottom.debugText('box' + this.name + ' x: ' + this.x + ' y: ' + this.y);
+            this.onKineticDown != undefined && 
+                this.scene.input.on('kineticDown', this.onKineticDown, this); 
+            this.onKineticUp != undefined && 
+                this.scene.input.on('kineticUp', this.onKineticUp, this); 
+            this.onKineticClick != undefined && 
+                this.scene.input.on('kineticClick', this.onKineticClick, this); 
+
+            this.scene.input.on('kineticOff', function(){
+                this.bottom.debugText('kineticOff: function off() ');
+                this.onKineticDown != undefined && 
+                    this.scene.input.off('kineticDown', this.onKineticDown, this); 
+                this.onKineticUp != undefined && 
+                    this.scene.input.off('kineticUp', this.onKineticUp, this); 
+                this.onKineticClick != undefined && 
+                    this.scene.input.off('kineticClick', this.onKineticClick, this); 
+                this.scene.input.off('kineticOff');
+            }, this)
+        }, gameobject);
+    }
+
+    stop(){
+
+        clearTimeout(this.timerDown);
+        clearTimeout(this.clearMovementTimer);
+
+        this.eventEmitter.off('step', this.update);
+        this.pressedDown = false;
+        this.scene.input.off('pointerdown', this.beginMove, this);
+        this.scene.input.off('pointermove', this.moveCamera, this);
+        this.scene.input.off('pointerup', this.endMove, this);
+        this.game.events.off('mouseout', this.endMove, this);
+
+        if (window.addEventListener)
+        {
+            window.removeEventListener('DOMMouseScroll', this.mouseWheel.bind(this), false);
+            window.onmousewheel = null;
+        }
+    }
+
+    configure(data){
+        //Object.assign(this, data);
+        if (data) {
+            for (var property in data) {
+                if (this.settings.hasOwnProperty(property)) {
+                    this.settings[property] = data[property];
+                }
+            }
+        }
+    }
+}
+
+if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+        exports = module.exports = KineticScrolling;
+    }
+    exports.KineticScrolling = KineticScrolling;
+} else if (typeof define !== 'undefined' && define.amd) {
+    define('KineticScrolling', (function() { return root.KineticScrolling = KineticScrolling; })() );
+} else {
+    root.KineticScrolling = KineticScrolling;
+}
+
+return KineticScrolling;
+
+
+}).call(this);
